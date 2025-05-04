f3c533029dd0d2428e26ce285ab6977c1954ec75
Kyle Schmottlach <kyleschmottlach@gmail.com>
2025-05-05 01:10:08 +0200

    Created linux install script

---
+++ scripts/setupLinux.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -e
+
+cd ..
+
+#Setup backend
+if [ ! -d "./.venv" ]; then
+  python3.13 -m venv .venv
+fi
+
+source ./.venv/bin/activate
+
+pip install --upgrade pip
+
+pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+pip install opencv-python-headless Flask pillow numpy gunicorn cython flask_cors
+pip install --no-build-isolation 'git+https://github.com/facebookresearch/detectron2.git'
+
+#Setup frontend
+
+if [ $(which node &> /dev/null; echo $?) -eq 1 ]; then
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+  \. "$HOME/.nvm/nvm.sh"
+  nvm install 23
+  nvm default 23
+else
+  if [ ! $(node --version | cut -c 2-3) -eq "23" ]; then
+    echo "Please install major node version 23"
+    exit 1
+  fi
+fi
+
+corepack enable yarn
+yarn set version latest
+yarn install
