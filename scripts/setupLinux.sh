cdf5b29846eb79583c1394e5ae7413e4c07ea83f
Kyle Schmottlach <kyleschmottlach@gmail.com>
2025-05-05 02:06:08 +0200

    Fixed Linux install script

--- scripts/setupLinux.sh
+++ scripts/setupLinux.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+#Install OS dependencies
+sudo apt update
+sudo apt install python3.13 python3.13-venv python3.13-dev git gcc g++ curl
+
 cd ..
 
 #Setup backend
@@ -23,7 +27,7 @@ if [ $(which node &> /dev/null; echo $?) -eq 1 ]; then
   curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
   \. "$HOME/.nvm/nvm.sh"
   nvm install 23
-  nvm default 23
+  nvm alias default 23
 else
   if [ ! $(node --version | cut -c 2-3) -eq "23" ]; then
     echo "Please install major node version 23"
@@ -32,5 +36,5 @@ else
 fi
 
 corepack enable yarn
-yarn set version latest
+yarn set version stable
 yarn install
