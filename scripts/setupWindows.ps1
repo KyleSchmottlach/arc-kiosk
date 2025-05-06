ba5a99dbcf785a8c8f802e16dcf784b4b140f876
Kyle Schmottlach <kyleschmottlach@gmail.com>
2025-05-06 23:03:06 +0200

    Hopefully fully updated windows setup script

--- scripts/setupWindows.ps1
+++ scripts/setupWindows.ps1
@@ -4,6 +4,23 @@ function RefreshEnv {
     $env:Path = $userpath + ";" + $machinePath
 }
 
+function Install-Node {
+    $fnmInstalled = Get-Command fnm -ErrorAction SilentlyContinue
+    if(!$fnmInstalled) {
+        winget install Schniz.fnm
+        if (-not (Test-Path $profile)) { New-Item $profile -Force }
+
+        Add-Content --Path "$HOME\Documents\PowerShell\Microsoft.PowerShell_profile.ps1" --Value "fnm env --use-on-cd --shell powershell | Out-String | Invoke-Expression"
+    }
+
+    RefreshEnv
+
+    fnm install 23
+    fnm default 23
+}
+
+Set-Location ..
+
 #Install Python
 $targetVersion = "3.13"
 $pythonInstalled = Get-Command python -ErrorAction SilentlyContinue
@@ -25,33 +42,11 @@ if (!$pythonInstalled) {
 
 winget install Microsoft.VisualStudio.2022.BuildTools --force --override "--wait --passive --add Microsoft.VisualStudio.Component.VC.Tools.x86.x64 --add Microsoft.VisualStudio.Component.Windows11SDK.26100 --add Microsoft.VisualStudio.Component.VC.CMake.Project"
 
-Set-Location ..
-
 if(!(Test-Path "./.venv")) {
-    python -m venv .venv
+    Remove-Item "./.venv"
 }
 
-$targetNodeVersion = "23"
-
-$nodeInstalled = Get-Command node -ErrorAction SilentlyContinue
-
-if(!$nodeInstalled) {
-    Write-Output "Node not installed. Installing using FNM..."
-    winget install Schniz.fnm
-
-    RefreshEnv
-
-    fnm install 23
-    fnm default 23
-
-    if (-not (Test-Path $profile)) { New-Item $profile -Force }
-
-    Add-Content --Path "$HOME\Documents\PowerShell\Microsoft.PowerShell_profile.ps1" --Value "fnm env --use-on-cd --shell powershell | Out-String | Invoke-Expression"
-
-    Invoke-Item $profile
-
-    RefreshEnv
-}
+python -m venv .venv
 
 . .\.venv\Scripts\Activate.ps1
 
@@ -61,3 +56,26 @@ pip install torch torchvision torchaudio --index-url https://download.pytorch.or
 pip install opencv-python-headless Flask pillow numpy gunicorn cython flask_cors
 pip install --no-build-isolation 'git+https://github.com/facebookresearch/detectron2.git'
 
+$targetNodeVersion = "23"
+
+$nodeInstalled = Get-Command node -ErrorAction SilentlyContinue
+
+if(!$nodeInstalled) {
+    Write-Output "Node not installed. Installing using FNM..."
+
+    Install-Node
+} else {
+    $output = node --version 2>&1
+
+    if($output -match "v\d+\") {
+        $installedVersion = $Matches[0].Substring(1)
+        if($installedVersion -ne $targetNodeVersion) {
+            Write-Output "Node version incompatible. Installing Node version 23 using fnm..."
+            Install-Node
+        }
+    }
+}
+
+corepack enable yarn
+yarn set version stable
+yarn install
